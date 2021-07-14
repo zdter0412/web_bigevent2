de3b4166dbd30a145607cd10c194045689d97141
zdter <18780190937@163.com>
2021-07-14 17:49:27 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,4 +2,22 @@
 $.ajaxPrefilter(function(options) {
     options.url = "http://api-breakingnews-web.itheima.net" + options.url;
     console.log(options.url);
+
+    // 统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载complete回调函数
+    options.complete = function(res) {
+        // 在complete回调函数中，可以用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token')
+                // 强制跳转到登录界面
+            location.href = '/login.html'
+        }
+
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+$(function() {
+        getUserInfo();
+        var layer = layui.layer;
+        $('#btnLogout').on('click', function() {
+            // 提示用户是否确认退出
+
+            layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+                //do something
+                // 清空本地存储中的token
+                localStorage.removeItem('token')
+                    // 重新跳转到登录界面
+                location.href = '/login.html'
+                layer.close(index);
+            });
+
+        })
+    })
+    // 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // header就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用函数渲染用户的头像
+            renderAvatar(res.data)
+
+        },
+        // 在jQuery发起ajax请求时，无论成功或者失败都会调用complete回调函数
+        // complete: function(res) {
+        //     // 在complete回调函数中，可以用res.responseJSON拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 强制清空token
+        //         localStorage.removeItem('token')
+        //             // 强制跳转到登录界面
+        //         location.href = '/login.html'
+        //     }
+
+
+        // }
+
+    })
+}
+
+function renderAvatar(user) {
+    // 获取用户的名称
+    var name = user.nickname || user.username
+        // 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).shpw();
+        $('.text-avatar').hide()
+    } else {
+        // 渲染文本头像
+        $('.layui-nav-img').hide();
+        // 获取文本字符串当中的第一个字符
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
